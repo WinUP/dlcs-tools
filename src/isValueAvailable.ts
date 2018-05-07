fa0f86da0bb2924715f5d4dbe99d2e4722244f3b
WinUP <l.gholoi@outlook.com>
2018-05-07 11:48:05 -0400

    Add Generic support for isValueAvailable.

--- src/isValueAvailable.ts
+++ src/isValueAvailable.ts
@@ -2,6 +2,6 @@
  * Indicate if target is not null, undefined, or empty string
  * @param data Target data
  */
-export function isValueAvailable(data: any): data is NonNullable<any> {
-    return data != null && data !== '';
+export function isValueAvailable<T = any>(data: T): data is NonNullable<T> {
+    return data != null && (typeof data === 'string' && data !== '');
 }
