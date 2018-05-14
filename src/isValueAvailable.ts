ca705b643e4d2a9fde1288ba7a7a3bebdab06f67
WinUP <l.gholoi@outlook.com>
2018-05-14 18:33:06 -0400

    Downgrade tsc version.

--- src/isValueAvailable.ts
+++ src/isValueAvailable.ts
@@ -2,6 +2,6 @@
  * Indicate if target is not null, undefined, or empty string
  * @param data Target data
  */
-export function isValueAvailable<T = any>(data: T): data is NonNullable<T> {
+export function isValueAvailable<T = any>(data: T): boolean {
     return data != null && (typeof data === 'string' && data !== '');
 }
